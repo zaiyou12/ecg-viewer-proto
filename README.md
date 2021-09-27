04a8801d2bc649cb766e46a7ce6353a18fbce3d5
aaron-so <zaiyou12@gmail.com>
2021-09-27 17:31:57 +0900

    🎉 Begin a project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Check and Go USA
+
+Vue 3 + Typescript + Vite
+
+## Recommended IDE Setup
+
+- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
+
+## References
+
+- [Checklist site](https://humaan.com/checklist/)
+- [Techstack example](https://github.com/zaiyou12/more-jobs)
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld msg="Hello World!"/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: rgba(167, 243, 208, 0.4);
+}
+.max-w-screen-lg {
+  max-width: 1012px;
+}
+</style>
